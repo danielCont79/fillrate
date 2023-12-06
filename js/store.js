86369e0baff69dfd29168c027ecc4bdb36a70ef0
Daniel Contreras <contreras.calderon@gmail.com>
2023-12-06 10:38:40 -0600

    .

--- js/store.js
+++ js/store.js
@@ -47,7 +47,7 @@ var store={
         {apiURL:_bkserver ,serviceName:"getSP/Generico?spname=VIS_Calcular_KPI_Flota_FillRate",varName:"flota",onInitLoad:false,useDateFilters:true},
         
 
-        {apiURL:_bkserver ,serviceName:"getSP/Generico?spname=VIS_Calcular_KPI_Venta_FillRate_Det",varName:"ventas",onInitLoad:false,useDateFilters:true},
+        {apiURL:_bkserver ,serviceName:"getSP/Generico?spname=VIS_Calcular_KPI_Venta_FillRate",varName:"ventas",onInitLoad:false,useDateFilters:true},
         {apiURL:_bkserver ,serviceName:"getSP/Generico?spname=VIS_Calcular_KPI_PedidosPendientes",varName:"pendientes",onInitLoad:false,useDateFilters:true},
         {apiURL:_bkserver ,serviceName:"getSP/VIS_Calcular_KPI_PedidosPendientes_Estado",varName:"pendientesEstado",onInitLoad:false,useDateFilters:true},
         {apiURL:_bkserver ,serviceName:"getSP/VIS_Calcular_KPI_Venta_FillRate_porDia_Det",varName:"ventasDia",onInitLoad:false,useDateFilters:false},
