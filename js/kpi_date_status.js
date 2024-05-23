105b4db03539f7320f80a2e7985280b3dce8e45f
Daniel Contreras <contreras.calderon@gmail.com>
2024-05-23 09:18:48 -0600

    .

--- js/kpi_date_status.js
+++ js/kpi_date_status.js
@@ -10,8 +10,8 @@ kpi_date_status.loadData = function(muestra ){
     var dateInit=new Date($('#datepicker').val());
     var dateEnd=new Date($('#datepicker2').val());
 
-    alert("Date picker value",$('#datepicker').val());
-    alert("date object",dateInit);
+    alert($('#datepicker').val().toString());
+    alert(dateInit.toString());
 
     var dateInit_=dateInit.getFullYear()+"-"+String(Number(dateInit.getMonth())+1)+"-"+dateInit.getDate();
     var dateEnd_=dateEnd.getFullYear()+"-"+String(Number(dateEnd.getMonth())+1)+"-"+dateEnd.getDate();
